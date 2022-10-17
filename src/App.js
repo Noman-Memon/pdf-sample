cab639b315fd5a1e9306d74b1eb41c350588a70f
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-10-17 14:17:54 +0500

    create pdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import { PDFViewer } from '@react-pdf/renderer'
+import MyDocument from './screen/Pdf'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <PDFViewer width="100%" height="1000px">
+      <MyDocument />
+    </PDFViewer>
+  )
 }
 
-export default App;
+export default App
